adb5bdf396b182802cd5793e3c369adab4b353b3
Yuval Roth <rothyuv@post.bgu.ac.il>
2024-06-10 19:37:46 +0300

    updated ms_nba

--- ms_nba/src/main/java/com/arealcompany/ms_nba/api/NbaApi.java
+++ ms_nba/src/main/java/com/arealcompany/ms_nba/api/NbaApi.java
@@ -1,9 +1,7 @@
 package com.arealcompany.ms_nba.api;
 
 import com.arealcompany.ms_nba.business.NbaController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class NbaApi {
@@ -14,13 +12,25 @@ public class NbaApi {
         this.controller = nbaController;
     }
 
-    @GetMapping("/teams")
+    @GetMapping("/get/teams")
     public String getTeams(@RequestParam(value = "limit", defaultValue = "-1") Integer limit) {
         return controller.getTeams(limit);
     }
 
-    @GetMapping(value = "/players")
+    @GetMapping(value = "/get/players")
     public String getPlayers(@RequestParam(value = "limit", defaultValue = "-1") Integer limit) {
         return controller.getPlayers(limit);
     }
+
+    @PostMapping("/update/teams")
+    public String updateTeams(@RequestBody String body) {
+        return controller.updateTeam(body);
+
+    }
+
+    @PostMapping("/update/players")
+    public String updatePlayers(@RequestBody String body) {
+        return controller.updatePlayer(body);
+    }
+
 }
