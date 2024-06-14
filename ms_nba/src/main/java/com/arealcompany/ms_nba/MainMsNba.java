535b19990bf4e78d9633137102dab9bc6d56d124
Yuval Roth <rothyuv@post.bgu.ac.il>
2024-06-14 12:49:18 +0300

    merge

--- ms_nba/src/main/java/com/arealcompany/ms_nba/MainMsNba.java
+++ ms_nba/src/main/java/com/arealcompany/ms_nba/MainMsNba.java
@@ -1,15 +1,15 @@
-package com.arealcompany.ms_population;
+package com.arealcompany.ms_nba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@EnableMongoRepositories(basePackages = "com.arealcompany.ms_population")
+@EnableMongoRepositories(basePackages = "com.arealcompany.ms_nba")
 @SpringBootApplication
 @EnableDiscoveryClient
-public class Main {
+public class MainMsNba {
     public static void main(String[] args) {
-		SpringApplication.run(Main.class, args);
+		SpringApplication.run(MainMsNba.class, args);
 	}
 }
