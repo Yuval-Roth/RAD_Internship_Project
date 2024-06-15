d52ad2def838eafdfdf9ad84126ce480872a8347
Yuval Roth <rothyuv@post.bgu.ac.il>
2024-06-15 15:03:47 +0300

    small update

--- elk/start_elk.bat
+++ elk/start_elk.bat
@@ -1,3 +1,3 @@
-start C:\Users\Yuval\git\RAD_Internship_Project\elk\elasticsearch\bin\elasticsearch.bat
-start C:\Users\Yuval\git\RAD_Internship_Project\elk\kibana\bin\kibana.bat
-start C:\Users\Yuval\git\RAD_Internship_Project\elk\logstash\bin\logstash.bat
+start "Elastic Search" elasticsearch\bin\elasticsearch.bat 
+start "Kibana" kibana\bin\kibana.bat
+start "Logstash" logstash\bin\logstash.bat -f config/logstash.conf
