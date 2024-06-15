ba497e6ee103887f5dae38cb6ec320ab3ceff2da
Yuval Roth <rothyuv@post.bgu.ac.il>
2024-06-15 14:48:22 +0300

    started tracking elk configs

---
+++ elk/start_elk.bat
@@ -0,0 +1,3 @@
+start C:\Users\Yuval\git\RAD_Internship_Project\elk\elasticsearch\bin\elasticsearch.bat
+start C:\Users\Yuval\git\RAD_Internship_Project\elk\kibana\bin\kibana.bat
+start C:\Users\Yuval\git\RAD_Internship_Project\elk\logstash\bin\logstash.bat
