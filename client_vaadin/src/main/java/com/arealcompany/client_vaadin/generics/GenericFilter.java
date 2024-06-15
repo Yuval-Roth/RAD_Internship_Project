860ed139bf82a921a3956758422dca9017e5ca35
Yuval Roth <rothyuv@post.bgu.ac.il>
2024-06-15 05:14:49 +0300

    greatly simplified the generic filter

--- client_vaadin/src/main/java/com/arealcompany/client_vaadin/generics/GenericFilter.java
+++ client_vaadin/src/main/java/com/arealcompany/client_vaadin/generics/GenericFilter.java
@@ -7,74 +7,38 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.textfield.TextFieldVariant;
 import com.vaadin.flow.data.value.ValueChangeMode;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class GenericFilter<T> {
 
     private final GridListDataView<T> dataView;
-    private final Class<T> clazz;
-    private final BiFunction<T,T,Boolean> filter;
-    private final Map<String, Field> fields;
-    private final T entity;
+    private final BiFunction<Function<String,String>,T,Boolean> filter;
+    private final Map<String,String> fieldsToValues;
 
-    public GenericFilter(GridListDataView<T> dataView, Class<T> clazz, BiFunction<T,T,Boolean> filter, String ... fields){
+    public GenericFilter(GridListDataView<T> dataView, BiFunction<Function<String,String>,T,Boolean> filter){
         this.dataView = dataView;
-        this.clazz = clazz;
         this.filter = filter;
-        this.fields = new HashMap<>();
-        try {
-            entity = clazz.getConstructor().newInstance();
-            for(String fieldName : fields){
-                Field field = clazz.getDeclaredField(fieldName);
-                field.setAccessible(true);
-                this.fields.put(fieldName,field);
-            }
-        } catch (NoSuchMethodException e) {
-            throw new RuntimeException("No default constructor found for class " + clazz.getName());
-        } catch (InvocationTargetException | IllegalAccessException | InstantiationException e) {
-            throw new RuntimeException("Could not instantiate class " + clazz.getName(),e);
-        } catch (NoSuchFieldException e) {
-            throw new RuntimeException("Could not find field in class " + clazz.getName(),e);
-        }
+        fieldsToValues = new HashMap<>();
         dataView.setFilter(this::filter);
     }
 
     public Component getFilterComponent(String fieldName){
-        if(fields.get(fieldName) == null){
-            throw new RuntimeException("Field " + fieldName + " not found in class " + clazz.getName());
-        }
         return createFilterComponent(value -> updateField(fieldName,value));
     }
 
     private boolean filter(T otherEntity){
-        return filter.apply(this.entity,otherEntity);
+        return filter.apply(fieldsToValues::get,otherEntity);
     }
 
-    private void updateField(String fieldName, Object value){
-        Field field = fields.get(fieldName);
-        if(field == null){
-            throw new RuntimeException("Field " + fieldName + " not found in class " + clazz.getName());
-        }
-        try {
-            assert value instanceof String;
-            String str = (String) value;
-            if(field.getType() == Integer.class){
-                try{
-                    value = Integer.parseInt(str);
-                } catch (NumberFormatException e){
-                    value = null;
-                }
-            } else if(field.getType() == Boolean.class){
-                value = value.equals("true") ? Boolean.TRUE : value.equals("false") ? Boolean.FALSE : null;
-            }
-            field.set(entity,value);
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException("Could not set field " + fieldName + " in class " + clazz.getName(),e);
+    private void updateField(String fieldName, String value){
+        if(value == null || value.isBlank()){
+            fieldsToValues.remove(fieldName);
+        } else {
+            fieldsToValues.put(fieldName, value);
         }
         dataView.refreshAll();
     }
--- client_vaadin/src/main/java/com/arealcompany/client_vaadin/generics/ListGenericView.java
+++ client_vaadin/src/main/java/com/arealcompany/client_vaadin/generics/ListGenericView.java
@@ -3,49 +3,65 @@ package com.arealcompany.client_vaadin.generics;
 import com.arealcompany.client_vaadin.Business.AppController;
 import com.arealcompany.client_vaadin.Business.Endpoints;
 import com.arealcompany.client_vaadin.exceptions.ApplicationException;
-import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.HeaderRow;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.component.textfield.TextFieldVariant;
-import com.vaadin.flow.data.value.ValueChangeMode;
 
 import java.lang.reflect.Field;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class ListGenericView<T> extends VerticalLayout {
 
     private final AppController appController;
     private final Map<String, Endpoints> endpoints;
-    private final Class<T> clazz;
     private final Grid<T> grid;
     private final GenericForm<T> form;
     private final List<String> displayFields;
     private Consumer<String> errorHandler;
+    private final Map<String,Function<Object,String>> fieldConverters;
 
     public ListGenericView(AppController appController, Class<T> clazz, Map<String, Endpoints> endpoints , List<String> displayFields) {
         this.appController = appController;
-        this.clazz = clazz;
         this.endpoints = endpoints;
         this.displayFields = displayFields;
         form = new GenericForm<>(clazz);
         grid = new Grid<>();
+        fieldConverters = new HashMap<>();
         configureGrid();
-        configureForm();
+        configureEditor();
         add(getContent());
         closeEditor();
         updateList(); // Initially populate the grid
     }
 
-    private void configureGrid() {
+    public ListGenericView<T> setErrorHandler(Consumer<String> handler) {
+        errorHandler = handler;
+        return this;
+    }
 
+    public ListGenericView<T> setFilter(BiFunction<Function<String,String>,T,Boolean> filterFunc, List<String> filterFields){
+        GenericFilter<T> filter = new GenericFilter<>(grid.getListDataView(), filterFunc);
+        HeaderRow hr = grid.appendHeaderRow();
+        for(String field : filterFields){
+            hr.getCell(grid.getColumnByKey(field)).setComponent(filter.getFilterComponent(field));
+        }
+        return this;
+    }
+
+    public ListGenericView<T> setFieldConverter(String fieldName, Function<Object,String> converter){
+        fieldConverters.put(fieldName,converter);
+        return this;
+    }
+
+    private void configureGrid() {
         // Dynamically set columns based on class fields and the specified display fields
         for (Iterator<String> iterator = displayFields.iterator(); iterator.hasNext(); ) {
             String fieldName = iterator.next();
@@ -53,7 +69,11 @@ public class ListGenericView<T> extends VerticalLayout {
                 try {
                     Field f = item.getClass().getDeclaredField(fieldName);
                     f.setAccessible(true);
-                    return f.get(item);
+                    if(fieldConverters.containsKey(fieldName)){
+                        return fieldConverters.get(fieldName).apply(f.get(item));
+                    } else {
+                        return f.get(item);
+                    }
                 } catch (NoSuchFieldException | IllegalAccessException e) {
                     throw new RuntimeException(e);
                 }
@@ -63,18 +83,6 @@ public class ListGenericView<T> extends VerticalLayout {
             }
             column.setSortable(true);
         }
-
-//        // Add filters
-//        HeaderRow filterRow = grid.appendHeaderRow();
-//        for (Field field : clazz.getDeclaredFields()) {
-//            if (displayFields.contains(field.getName())) {
-//                TextField filterField = new TextField();
-//                filterFields.put(field.getName(), filterField);
-//                addFilterToColumn(filterRow, grid.getColumnByKey(field.getName()), filterField);
-//            }
-//        }
-
-        // Add the edit button column
         grid.addComponentColumn(this::createEditButton).setHeader("Actions").setFlexGrow(0);
     }
 
@@ -89,60 +97,17 @@ public class ListGenericView<T> extends VerticalLayout {
         form.setVisible(true);
     }
 
-//    private void addFilterToColumn(HeaderRow filterRow, Grid.Column<T> column, TextField filterField) {
-//        if (column != null) {
-//            filterField.setPlaceholder("Filter...");
-//            filterField.setClearButtonVisible(true);
-//            filterField.setValueChangeMode(ValueChangeMode.LAZY);
-//            filterField.addValueChangeListener(e -> updateList());
-//            filterRow.getCell(column).setComponent(filterField);
-//        }
-//    }
-
-    //TODO: this is extremely wasteful, we can filter the list without
-    // fetching all the data all the time.
-    // You can see how it's done in the views classes
-    private void updateList() {
-        try {
-            List<T> items = appController.fetchByEndpoint(endpoints.get("fetch"));
-//            List<T> filteredData = items.stream()
-//                    .filter(entity -> filterFields.entrySet().stream().allMatch(entry -> {
-//                        String fieldName = entry.getKey();
-//                        String filterValue = entry.getValue().getValue();
-//                        try {
-//                            Field field = entity.getClass().getDeclaredField(fieldName);
-//                            field.setAccessible(true);
-//                            Object value = field.get(entity);
-//                            return filterValue == null || filterValue.isEmpty() ||
-//                                    (value != null
-//                                            && value.toString().toLowerCase().contains(filterValue.toLowerCase()));
-//                        } catch (Exception e) {
-//                            return true;
-//                        }
-//                    }))
-//                    .collect(Collectors.toList());
-//            grid.setItems(filteredData);
-            grid.setItems(items);
-        } catch (ApplicationException e) {
-            handleError(e.getMessage());
-        }
-    }
-
-    public void setErrorHandler(Consumer<String> handler) {
-        errorHandler = handler;
-    }
-
-    public void setFilter(BiFunction<T,T,Boolean> filterFunc){
-        GenericFilter<T> filter = new GenericFilter<>(grid.getListDataView(), clazz, filterFunc, displayFields.toArray(new String[0]));
-        HeaderRow hr = grid.appendHeaderRow();
-        for(String field : displayFields){
-            hr.getCell(grid.getColumnByKey(field)).setComponent(filter.getFilterComponent(field));
-        }
-    }
-
     private void handleError(String message) {
         if(errorHandler != null) {
             errorHandler.accept(message);
+        }
+    }
+
+    private void updateList() {
+        try {
+            grid.setItems(appController.fetchByEndpoint(endpoints.get("fetch")));
+        } catch (ApplicationException e) {
+            handleError(e.getMessage());
         }
     }
 
@@ -153,7 +118,7 @@ public class ListGenericView<T> extends VerticalLayout {
         return content;
     }
 
-    private void configureForm() {
+    private void configureEditor() {
         form.setWidth("25em");
         form.addSaveListener(this::updateEntity);
         form.addDeleteListener(this::deleteEntity);
@@ -184,17 +149,7 @@ public class ListGenericView<T> extends VerticalLayout {
         form.setVisible(false);
     }
 
-    private Component createFilterHeader(Consumer<String> filterChangeConsumer) {
-        TextField textField = new TextField();
-        textField.setPlaceholder("Filter");
-        textField.setValueChangeMode(ValueChangeMode.EAGER);
-        textField.setClearButtonVisible(true);
-        textField.addThemeVariants(TextFieldVariant.LUMO_SMALL);
-        textField.addValueChangeListener(
-                e -> filterChangeConsumer.accept(e.getValue()));
-        VerticalLayout layout = new VerticalLayout(textField);
-        layout.getThemeList().clear();
-        layout.getThemeList().add("spacing-xs");
-        return layout;
+    public Grid<T> getGrid() {
+        return grid;
     }
 }
