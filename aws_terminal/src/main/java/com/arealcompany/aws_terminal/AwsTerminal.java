db9f30bc4034fd163971958b0eaf5b982309de19
Yuval Roth <rothyuv@post.bgu.ac.il>
2024-07-11 15:27:42 +0300

    added docker compose to terminal

--- aws_terminal/src/main/java/com/arealcompany/aws_terminal/AwsTerminal.java
+++ aws_terminal/src/main/java/com/arealcompany/aws_terminal/AwsTerminal.java
@@ -4,11 +4,21 @@ import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.ec2.Ec2Client;
 import software.amazon.awssdk.services.ec2.model.*;
 
+import java.util.Map;
+
 public class AwsTerminal {
 
     private static final String INSTANCE_TYPE = "t3.micro";
     private static Ec2Client ec2;
     private static final String[] services = new String[]{"ms-api-gateway", "ms-news", "ms-population", "ms-nba", "vaadin-client"};
+    private static final Map<String,Integer> ports = Map.of(
+            "ms-api-gateway", 8080,
+            "ms-news", null,
+            "ms-population", null,
+            "ms-nba", null,
+            "vaadin-client", 80
+    );
+    private static String imageId;
 
     public static void main(String[] args) {
         ec2 = Ec2Client.builder()
@@ -21,25 +31,18 @@ public class AwsTerminal {
                         .values("Docker")
                         .build()).build();
         DescribeImagesResponse imagesResponse = ec2.describeImages(describeImagesRequest);
-        String imageId = imagesResponse.images().getFirst().imageId();
+        imageId = imagesResponse.images().getFirst().imageId();
 
         while(true){
             try{
-                System.out.println("Booting up services");
-                for(String service : services){
-                    runIfNotRunning(imageId, service);
-                }
-
                 System.out.println("AWS Terminal");
-                System.out.println("1. List instances");
-                System.out.println("2. Start instances");
-                System.out.println("3. Shutdown instances");
-                System.out.println("4. Exit");
+                System.out.println("1. Start services");
+                System.out.println("2. Shutdown services");
+                System.out.println("3. Exit");
                 String answer = System.console().readLine();
                 switch(answer){
-                    case "1" -> listInstances();
-                    case "2" -> startInstances();
-                    case "3" -> shutdownInstances();
+                    case "2" -> startServices();
+                    case "3" -> shutdownServices();
                     case "4" -> exit();
                 }
             } catch(Exception e){
@@ -48,18 +51,21 @@ public class AwsTerminal {
         }
     }
 
-    private static void startInstances() {
+    private static void startServices() {
+        System.out.println("Booting up services");
+        for(String service : services){
+            runIfNotRunning(imageId, service);
+        }
     }
 
     private static void exit() {
     }
 
-    private static void shutdownInstances() {
+    private static void shutdownServices() {
 
     }
 
     private static void listInstances() {
-
     }
 
     private static void runIfNotRunning(String imageId, String service) {
@@ -99,9 +105,6 @@ public class AwsTerminal {
     private static String getUserScript(String imageName, Integer port){
         return """
                 #!/bin/bash
-                docker run --rm %s %s""".formatted(
-                        port != null ? "-p %d:%d".formatted(port,port) : "",
-                        imageName
-        );
+                wget """
     }
 }
