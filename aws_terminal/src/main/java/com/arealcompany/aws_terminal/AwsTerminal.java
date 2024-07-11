14116b0e33d52fff29f1fce6cca19f147705fc00
Yuval Roth <rothyuv@post.bgu.ac.il>
2024-07-11 16:08:39 +0300

    updated aws_terminal

--- aws_terminal/src/main/java/com/arealcompany/aws_terminal/AwsTerminal.java
+++ aws_terminal/src/main/java/com/arealcompany/aws_terminal/AwsTerminal.java
@@ -4,21 +4,17 @@ import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.ec2.Ec2Client;
 import software.amazon.awssdk.services.ec2.model.*;
 
-import java.util.Map;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.List;
 
 public class AwsTerminal {
 
     private static final String INSTANCE_TYPE = "t3.micro";
+    private static final String INSTANCE_NAME = "RAD_microservices";
     private static Ec2Client ec2;
-    private static final String[] services = new String[]{"ms-api-gateway", "ms-news", "ms-population", "ms-nba", "vaadin-client"};
-    private static final Map<String,Integer> ports = Map.of(
-            "ms-api-gateway", 8080,
-            "ms-news", null,
-            "ms-population", null,
-            "ms-nba", null,
-            "vaadin-client", 80
-    );
     private static String imageId;
+    private static final String SECURITY_GROUP = "sg-0befc037eb4342677";
 
     public static void main(String[] args) {
         ec2 = Ec2Client.builder()
@@ -28,7 +24,7 @@ public class AwsTerminal {
         DescribeImagesRequest describeImagesRequest = DescribeImagesRequest.builder()
                 .filters(Filter.builder()
                         .name("tag:Name")
-                        .values("Docker")
+                        .values("docker")
                         .build()).build();
         DescribeImagesResponse imagesResponse = ec2.describeImages(describeImagesRequest);
         imageId = imagesResponse.images().getFirst().imageId();
@@ -41,9 +37,9 @@ public class AwsTerminal {
                 System.out.println("3. Exit");
                 String answer = System.console().readLine();
                 switch(answer){
-                    case "2" -> startServices();
-                    case "3" -> shutdownServices();
-                    case "4" -> exit();
+                    case "1" -> startServices();
+                    case "2" -> shutdownServices();
+                    case "3" -> exit();
                 }
             } catch(Exception e){
                 e.printStackTrace();
@@ -53,9 +49,7 @@ public class AwsTerminal {
 
     private static void startServices() {
         System.out.println("Booting up services");
-        for(String service : services){
-            runIfNotRunning(imageId, service);
-        }
+        runIfNotRunning(imageId, INSTANCE_NAME);
     }
 
     private static void exit() {
@@ -65,46 +59,43 @@ public class AwsTerminal {
 
     }
 
-    private static void listInstances() {
-    }
-
-    private static void runIfNotRunning(String imageId, String service) {
-        DescribeInstancesRequest describeInstancesRequest = DescribeInstancesRequest.builder()
-                .filters(Filter.builder()
-                        .name("tag:Name")
-                        .values(service).build()).build();
-        DescribeInstancesResponse instancesResponse = ec2.describeInstances(describeInstancesRequest);
-        if(instancesResponse.hasReservations()){
+    private static void runIfNotRunning(String imageId, String machineName) {
+        if(isRunning()){
             return;
         }
-
-        System.out.printf("Starting %s%n", service);
+        System.out.printf("Starting %s%n", machineName);
 
         RunInstancesRequest runRequest = RunInstancesRequest.builder()
                 .imageId(imageId)
+                .securityGroupIds(SECURITY_GROUP)
                 .instanceType(INSTANCE_TYPE)
                 .maxCount(1)
                 .minCount(1)
-                .userData(getUserScript(service))
+                .userData(new String(Base64.getEncoder().encode(getUserScript().getBytes())))
                 .tagSpecifications(TagSpecification.builder()
                         .resourceType(ResourceType.INSTANCE)
                         .tags(Tag.builder()
                                 .key("Name")
-                                .value(service)
+                                .value(machineName)
                                 .build())
                         .build())
                 .build();
-
         ec2.runInstances(runRequest);
     }
 
-    private static String getUserScript(String imageName){
-        return getUserScript(imageName, null);
+    private static boolean isRunning() {
+        return (int) ec2.describeInstances().reservations().stream()
+                .flatMap(reservation -> reservation.instances().stream())
+                .filter(instance -> instance.state().name() != InstanceStateName.TERMINATED)
+                .filter(instance -> instance.tags().stream()
+                        .anyMatch(tag -> tag.key().equals("Name") && tag.value().equals(INSTANCE_NAME)))
+                .count() > 0;
     }
 
-    private static String getUserScript(String imageName, Integer port){
+    private static String getUserScript(){
         return """
                 #!/bin/bash
-                wget """
+                wget https://raw.githubusercontent.com/Yuval-Roth/RAD_Internship_Project/docker/aws_terminal/docker-compose.yml -O /~/docker-compose.yml
+                cd ~/ && docker-compose up""";
     }
 }
